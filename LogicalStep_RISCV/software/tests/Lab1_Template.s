77a4d5e37bf6c253dccf570bc72a27df9c7ceb63
David Han Zhu <kunal_chandan@outlook.com>
2020-01-14 15:00:34 -0500

    inital upload from desktop

--- LogicalStep_RISCV/software/tests/Lab1_Template.s
+++ LogicalStep_RISCV/software/tests/Lab1_Template.s
@@ -8,23 +8,25 @@
 .text
 .globl	main
 main:
-	# Put your initializations here
-	lui s1, 0x7ff60 			# assigns s1 with the LED base address (Could be replaced with lui s1, 0x7ff60)
-	addi s2, zero, 0x55			# assigns s2 with the 0x55 which will be later loaded to the LED to turn it on (0101 0101 lights)
-	sw s2, 0(s1)				# storing s2 value to the LED base address to turn the LED on (you can delete this line along side with the prevuos one if you want, just for illustration on how to turn on and off the LED) 
-	#li s3, 11					# sets the counter max, you need to change that number to make the 500ms delay
-	addi s3, zero, 0xAA			# (1010 1010 lights)
-	sw s3, 0(s1)
-    # an infinite loop, almost needed in any embedded systems code
-	while_1:
-		# You might write some code here
-		#
-		
-		
-		
-		
-		# You might write some code here 
-		# Also you might write another delay routine if using the long flowchart given in the lab manual
-		
-		
-		j while_1				# j label is a pseudo-instruction equivalent to beq x0,x0,label
+	lui s1, 0x7FF60 ## Stores the most 20 sig bits as argument 0x7FF60
+    addi s2, zero, 0xFF ## Stores the 8 least sig bits as 0xFF
+    sw s2, 0(s1) ## store 0xFF into s2 least sig bits so that it turns on
+    ## s3 is counter
+    ## TODO:: Re-adjust this value based on # of instructions in loop
+    li s4, 0x5F5E10
+    li s5, 0x55
+	
+    TOGGLE:
+    	## TOGGLE LEDS
+    	xori s5, s5, 0xFF
+        ## LOAD into LEDS
+    	sw s5, 0(s1)
+        ## RESET COUNTER
+        li s3, 0
+        j while_0
+
+    while_0:
+    	addi s3, s3, 1
+        
+        BNE s3, s4, while_0
+        BEQ s3, s4, TOGGLE
